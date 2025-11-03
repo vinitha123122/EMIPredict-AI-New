07a8fe06b6efa19aed80d12be2f13853df746ca6
Vinitha123122 <vinithab219@gmail.com>
2025-11-03 11:57:13 +0530

    Final pickle serialization fix and model rebuild for stable cloud deployment.

--- pages/pages-1_Prediction.py
+++ pages/pages-1_Prediction.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+import pickle
 import xgboost # Required to load XGBoost models
 
 # --- FIX for AttributeError: Can't get attribute '_RemainderColsList' ---
